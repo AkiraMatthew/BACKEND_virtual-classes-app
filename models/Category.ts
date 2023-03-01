e3981df95aa457b258cb74dcc8076b7628ebf71e
AkiraMatthew <akira.dev.math@gmail.com>
2023-02-28 22:34:32 -0300

    category model integration

--- models/Category.ts
+++ models/Category.ts
@@ -1,4 +1,5 @@
-import { Model, Optional } from "sequelize/types";
+import { DataTypes, Model, Optional } from "sequelize";
+import { sequelize } from "../src/database";
 
 export interface Category {
     id: number,
@@ -8,4 +9,22 @@ export interface Category {
 
 export interface CategoryCreationAttributes extends Optional<Category, 'id'>{  };
 
-export interface CategoryInstance extends Model<Category, CategoryCreationAttributes>, Category{  }
+export interface CategoryInstance extends Model<Category, CategoryCreationAttributes>, Category{  };
+
+export const Category = sequelize.define<CategoryInstance, Category> ('Category', {
+    id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: DataTypes.INTEGER
+    },
+        name: {
+        allowNull: false,
+        type: DataTypes.STRING
+    },
+        position: {
+        allowNull: false,
+        unique: true,
+        type: DataTypes.INTEGER
+    }
+})
