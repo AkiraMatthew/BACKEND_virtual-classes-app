6f77cfacdb5dcad775fb61b4d68a118b75f026c7
AkiraMatthew <akira.dev.math@gmail.com>
2023-03-05 16:43:39 -0300

    first route

--- src/controllers/categoriesController.ts
+++ src/controllers/categoriesController.ts
@@ -3,7 +3,18 @@ import { Category } from "../models";
 
 export const categoriesController = {
     index: async (req: Request, res: Response) => {
-        const categories = await Category.findAll();
-        res.json(categories)
+        try {
+                const categories = await Category.findAll({ //You can use the findAll propertiers to specify the behavior of the query response received in the frontend
+                attributes: ['id', 'name', 'position'],
+                order: [['position', 'ASC']]
+            });
+            
+            //throw new Error('database connection error')
+            return res.json(categories)
+        } catch (error) {
+            if(error instanceof Error){
+                return res.status(400).json({ message: error.message })
+            }
+        }
     }
 }
