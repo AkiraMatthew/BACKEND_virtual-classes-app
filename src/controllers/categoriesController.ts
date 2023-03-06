82086473b3d8f9cd9ff88b9b2d1a16fe9e3b8ae2
AkiraMatthew <akira.dev.math@gmail.com>
2023-03-06 19:39:16 -0300

    categories paging implementation

--- src/controllers/categoriesController.ts
+++ src/controllers/categoriesController.ts
@@ -3,14 +3,33 @@ import { Category } from "../models";
 
 export const categoriesController = {
     index: async (req: Request, res: Response) => {
+        const { page, perPage } = req.query;
+
+        const perPageNumber = typeof perPage === 'string' && parseInt(perPage, 10) > 0
+            ? parseInt(perPage, 10)
+            : 10
+  
+        const pageNumber = typeof page === 'string' && parseInt(page, 10) > 0
+            ? parseInt(page, 10)
+            : 1
+  
+        const offset = (pageNumber - 1) * perPageNumber
+
         try {
-                const categories = await Category.findAll({ //You can use the findAll propertiers to specify the behavior of the query response received in the frontend
+            const { count, rows } = await Category.findAndCountAll({ //You can use the findAll propertiers to specify the behavior of the query response received in the frontend
                 attributes: ['id', 'name', 'position'],
-                order: [['position', 'ASC']]
+                order: [['position', 'ASC']],
+                limit: perPageNumber,
+                offset
             });
             
             //throw new Error('database connection error')
-            return res.json(categories)
+            return res.json( { 
+                categories: rows,
+                page: pageNumber,
+                perPage: perPageNumber,
+                total: count
+             } )
         } catch (error) {
             if(error instanceof Error){
                 return res.status(400).json({ message: error.message })
