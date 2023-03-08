4c5ea1959980717b8641c84ebf005b9120e96eae
AkiraMatthew <akira.dev.math@gmail.com>
2023-03-08 00:54:41 -0300

    courses episodes controller, services and route

--- src/models/index.ts
+++ src/models/index.ts
@@ -5,10 +5,12 @@ import { Course } from './Course';
 import { Episode } from './Episode';
 import { User } from './User';
 
-Category.hasMany(Course, { as: 'courses' })
+//sequelize as default to name the target association with its default version
+Category.hasMany(Course, { as: 'courses' }) //on this query, rather than "{ as: 'courses' }", 
+//sequelize would convert the target Model name to be identified "{ as: 'Courses' }", with the target name on plural
 
 Course.belongsTo(Category)
-Course.hasMany(Episode)
+Course.hasMany(Episode) //in this case the target Model name would be "{ as: 'Episodes' }"
 
 Episode.belongsTo(Course)
 
--- src/routes.ts
+++ src/routes.ts
@@ -1,10 +1,12 @@
 import express from "express";
 import { categoriesController } from "./controllers/categoriesController";
+import { coursesController } from "./controllers/coursesController";
 
 const router = express.Router();
 
 //get routes - categories
 router.get('/categories', categoriesController.index);
-router.get('/categories/:id', categoriesController.show)
+router.get('/categories/:id', categoriesController.show);
+router.get('/courses/:id', coursesController.show);
 
 export { router }
