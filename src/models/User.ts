a7956bc149a378ea346c29ea408c54d6df4d6b71
AkiraMatthew <akira.dev.math@gmail.com>
2023-03-12 19:02:27 -0300

    password check User instance

--- src/models/User.ts
+++ src/models/User.ts
@@ -3,6 +3,7 @@
 import { sequelize } from '../database';
 import { DataTypes, Model, Optional } from 'sequelize';
 import bcrypt from 'bcrypt';
+import { type } from 'os';
 
 export interface User {
   id: number
@@ -19,7 +20,9 @@ export interface UserCreationAttributes
   extends Optional<User, 'id'> {}
 
 export interface UserInstance
-  extends Model<User, UserCreationAttributes>, User {}
+  extends Model<User, UserCreationAttributes>, User {
+    checkPassword: (password: string, callbackfn: checkPasswordCallback) => void;
+  }
 
 export const User = sequelize.define<UserInstance, User>('User', {
   id: {
@@ -71,4 +74,17 @@ export const User = sequelize.define<UserInstance, User>('User', {
       }
     }
   }
-})
+});
+
+// User instance for password  check
+type checkPasswordCallback = (err?: Error, isSame?: boolean)=>void;
+
+User.prototype.checkPassword = function (password: string, callbackfn: checkPasswordCallback){
+  bcrypt.compare(password, this.password, ((err, isSame) => {
+    if (err) {
+      callbackfn(err)
+    } else {
+      callbackfn(err, isSame)
+    }
+  }))
+}
