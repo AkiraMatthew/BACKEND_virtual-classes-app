fbc39cd2139519f5dbcd7efef67e315ba17f2b17
AkiraMatthew <akira.dev.math@gmail.com>
2023-03-14 19:20:19 -0300

    video authentication, need to fix the auth

--- src/middlewares/auth.ts
+++ src/middlewares/auth.ts
@@ -4,32 +4,76 @@ import { userService } from "../services/userService";
 import { JwtPayload } from "jsonwebtoken";
 import { UserInstance } from "../models/User";
 
-// code fix
+// code fixed
 
 export interface AuthenticatedRequest extends Request {
-    user?: UserInstance | null
+  user?: UserInstance | null;
+}
+
+export function ensureAuth(req: AuthenticatedRequest, res: Response, next: NextFunction) {
+  const authorizationHeader = req.headers.authorization;
+
+  if (!authorizationHeader) {
+    return res.status(401).json({ message: 'Unauthorized access: no token was found' });
   }
-  
-  export function ensureAuth(req: AuthenticatedRequest, res: Response, next: NextFunction) {
-    const authorizationHeader = req.headers.authorization
-  
-    if (!authorizationHeader) {
-      return res.status(401).json({ message: 'Não autorizado: nenhum token encontrado' })
+
+  const token = authorizationHeader.replace(/Bearer /, '');
+
+  // Import the jwtService module and verifyToken method
+  jwtService.verifyToken(token, async (err: any, decoded: any) => { // add types for err and decoded
+    if (err || typeof decoded === 'undefined') {
+      return res.status(401).json({ message: 'Unauthorized access: invalid token' });
     }
-  
-    const token = authorizationHeader.replace(/Bearer /, '')
-  
-    jwtService.verifyToken(token, (err, decoded) => {
-      if (err || typeof decoded === 'undefined') {
-        return res.status(401).json({ message: 'Não autorizado: token inválido' })
-      }
-  
-      userService.findByEmail((decoded as JwtPayload).email).then(user => {
-        req.user = user
-        next()
-      })
-    })
+
+    const user = await userService.findByEmail((decoded as JwtPayload).email);
+    req.user = user;
+    next();
+  });
+}
+
+export function ensureAuthViaQuery(req: AuthenticatedRequest, res: Response, next: NextFunction) {
+  const { token } = req.query;
+
+  if (!token) {
+    return res.status(401).json({ message: 'Unauthorized access: no token was found' });
   }
+
+  if (typeof token !== 'string') {
+    return res.status(400).json({ message: `The token argument must be of type 'string'` });
+  }
+
+  // Import the jwtService module and verifyToken method
+  jwtService.verifyToken(token, async (err: any, decoded: any) => { // add types for err and decoded
+    if (err || typeof decoded === 'undefined') {
+      return res.status(401).json({ message: 'Unauthorized access: invalid token' });
+    }
+
+    const user = await userService.findByEmail((decoded as JwtPayload).email)
+      req.user = user;
+      next();
+  });
+}
+/*export function ensureAuthViaQuery(req: AuthenticatedRequest, res: Response, next: NextFunction){
+  const { token } = req.query;
+
+  if(!token) {
+    return res.status(401).json({ message: 'Unauthorized access: no token was found' })
+  };
+
+  if(typeof token !== 'string'){
+    return res.status(401).json({ message: `The token argument must be of type 'string' `})
+  };
+
+  jwtService.verifyToken(token, async (err, decoded) => {
+    if (err || typeof decoded === 'undefined') {
+      return res.status(401).json({ message: 'Unauthorized access: invalid token' })
+    };
+
+    const user = await userService.findByEmail((decoded as JwtPayload).email);
+    req.user = user;
+    next();
+  })
+}*/
 
 //////////////////////////////////////////////////////////////////////////////////////////////////////////
 
