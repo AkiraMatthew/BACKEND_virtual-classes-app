43af080f03d614c218da450552c960836c9830fa
AkiraMatthew <akira.dev.math@gmail.com>
2023-03-14 02:20:10 -0300

    fixing token auth

--- src/middlewares/auth.ts
+++ src/middlewares/auth.ts
@@ -4,29 +4,63 @@ import { userService } from "../services/userService";
 import { JwtPayload } from "jsonwebtoken";
 import { UserInstance } from "../models/User";
 
-export interface AuthenticatedRequest extends Request{
+// code fix
+
+export interface AuthenticatedRequest extends Request {
+    user?: UserInstance | null
+  }
+  
+  export function ensureAuth(req: AuthenticatedRequest, res: Response, next: NextFunction) {
+    const authorizationHeader = req.headers.authorization
+  
+    if (!authorizationHeader) {
+      return res.status(401).json({ message: 'Não autorizado: nenhum token encontrado' })
+    }
+  
+    const token = authorizationHeader.replace(/Bearer /, '')
+  
+    jwtService.verifyToken(token, (err, decoded) => {
+      if (err || typeof decoded === 'undefined') {
+        return res.status(401).json({ message: 'Não autorizado: token inválido' })
+      }
+  
+      userService.findByEmail((decoded as JwtPayload).email).then(user => {
+        req.user = user
+        next()
+      })
+    })
+  }
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+/*export interface AuthenticatedRequest extends Request{
     user?: UserInstance | null
 }
 
 export function ensureAuth(req: AuthenticatedRequest, res: Response, next: NextFunction){
     const authorizationHeader = req.headers.authorization;
 
-    if(authorizationHeader === undefined) return res.status(401).json({
-        message: 'Unauthorized access: no token was found'
-    });
+    if(authorizationHeader === undefined) {
+        return res.status(401).json({
+            message: 'Unauthorized access: no token was found'
+        });
+    }
 
     // Bearer 'token' -> // Bearer ewredsewrwe
     const token = authorizationHeader.replace(/Bearer/, '');
     console.log(token);
 
-    jwtService.verifyToken(token, async (err, decoded) => {
-        if(err || typeof decoded === undefined) return res.status(401).json({
-            message: 'Unauthorized access: invalid token'
-        });
+    jwtService.verifyToken(token, (err, decoded) => {
+        if(err || typeof decoded === undefined) {
+            return res.status(401).json({
+                message: 'Unauthorized access: invalid token'
+            });
+        }
 
-        await userService.findByEmail((decoded as JwtPayload).email).then(user => {
+        userService.findByEmail((decoded as JwtPayload).email).then(user => {
             req.user = user;
             next()
         })
     })
-}
+}*/
